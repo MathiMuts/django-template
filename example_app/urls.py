3ad8969225b868f83b3713fdf831ee4b50d77961
Mathi <Mathias.meersschaut@icloud.com>
2025-08-02 16:44:23 +0200

    app

---
+++ example_app/urls.py
@@ -0,0 +1,10 @@
+# example_app/urls.py
+
+from django.urls import path
+from . import views
+
+app_name = 'example_app'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
--- example_app/views.py
+++ example_app/views.py
@@ -1,3 +1,6 @@
+# example_app/views.py
+
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'example_app/pages/index.html')
