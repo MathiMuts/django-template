5bcf970aeaa9f8778be89163ed1b7fcc8075ad82
Mathi <Mathias.meersschaut@icloud.com>
2025-08-02 20:34:55 +0200

    cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,6 @@ echo "Entrypoint script started as user: $(whoami)"
 
 echo "Creating environment file for cron jobs..."
 
-if [ -n "$DB_HOST" ] && [ -n "$DB_PORT" ]; then
-    echo "Waiting for PostgreSQL at $DB_HOST:$DB_PORT..."
-    while ! nc -z $DB_HOST $DB_PORT; do
-      sleep 0.1
-    done
-    echo "PostgreSQL started"
-fi
-
 echo "Applying database migrations..."
 python manage.py migrate --noinput
 
