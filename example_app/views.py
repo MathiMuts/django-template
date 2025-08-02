2510296acbc83e3e4fae74b8bc0995d377a4caca
Mathi <Mathias.meersschaut@icloud.com>
2025-08-02 16:52:18 +0200

    app

--- example_app/views.py
+++ example_app/views.py
@@ -1,6 +1,10 @@
 # example_app/views.py
 
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 
+@cache_page(10) # Cache page for 10s ( DO NOT USE FOR FORMS OR REACTIVE )
 def index(request):
-    return render(request, 'example_app/pages/index.html')
+    import time
+    print("Page served not from cache")
+    return render(request, 'example_app/pages/index.html', {'time': time.time}) # -> Time will only update every 10s
